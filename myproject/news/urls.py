13cdbb873ad2a3ee3e6ae1743eb2fc49c9bd1cfa
agent1156 <agent1156@yandex.ru>
2026-02-07 23:24:05 +0300

    2

--- myproject/news/urls.py
+++ myproject/news/urls.py
@@ -1,6 +1,8 @@
+# news/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('news/<int:news_id>/', views.news_detail, name='news_detail'),
 ]
--- myproject/news/views.py
+++ myproject/news/views.py
@@ -1,10 +1,50 @@
-from django.shortcuts import render
+
+
+# news/views.py
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import News
+
 
 def index(request):
-    """Главная страница приложения news"""
+    """Главная страница со списком новостей"""
+    news = News.objects.filter(status='published').order_by('-created_at')
     context = {
         'title': 'Новостной портал',
         'message': 'Добро пожаловать!',
-        'current_time': '19:30'
+        'news': news,
     }
     return render(request, 'news/index.html', context)
+
+
+def news_detail(request, news_id):
+    """
+    Детальная страница новости
+    :param request: HTTP запрос
+    :param news_id: ID новости
+    :return: Детальная страница новости
+    """
+    try:
+        # Получаем новость или возвращаем 404
+        news_item = get_object_or_404(News, id=news_id, status='published')
+
+        # Увеличиваем счетчик просмотров
+        news_item.views += 1
+        news_item.save(update_fields=['views'])
+
+        # Получаем похожие новости (из той же категории)
+        related_news = News.objects.filter(
+            category=news_item.category,
+            status='published'
+        ).exclude(id=news_id).order_by('-created_at')[:3]
+
+        context = {
+            'news': news_item,
+            'title': news_item.title,
+            'related_news': related_news,
+        }
+
+        return render(request, 'news/news_detail.html', context)
+
+    except News.DoesNotExist:
+        raise Http404("Новость не найдена")
