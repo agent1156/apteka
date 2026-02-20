4aca6bb0097cf6063e207d690e21e237e9112ebe
unknown <agent1156@yandex.ru>
2026-02-20 11:59:00 +0300

    catalog1

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
     path('accounts/', include('accounts.urls')),
+    path('catalog/', include('catalog.urls')),
 ]
