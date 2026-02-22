58a5377766b0c229588ae289058a3e14d7a0a838
agent1156 <agent1156@yandex.ru>
2026-02-22 18:27:08 +0300

    Визуал каталога

--- myproject/catalog/urls.py
+++ myproject/catalog/urls.py
@@ -1,7 +1,8 @@
-# news/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.product_list, name='product_list'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
+
 ]
--- myproject/catalog/views.py
+++ myproject/catalog/views.py
@@ -1,5 +1,10 @@
-# news/views.py
 from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-def index(request):
-    return render(request,'catalog/index.html')
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'catalog/product_list.html', {'products': products})
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'catalog/product_detail.html', {'product': product})
