e8d29b0ebf251ce60a3a95c078c8202dec55d4f3
unknown <agent1156@yandex.ru>
2026-03-06 00:25:59 +0300

    пагинация готова

--- myproject/catalog/views.py
+++ myproject/catalog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
-
+from django.core.paginator import Paginator
 def product_list(request):
     products = Product.objects.all()
-    return render(request, 'catalog/product_list.html', {'products': products})
+    paginator = Paginator(products, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'catalog/product_list.html', {'products': page_obj})
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, id=product_id)
